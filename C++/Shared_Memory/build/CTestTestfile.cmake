f3fcb91b25e4591171582bf19c0825b333d73071
yunjin544 <yunjin544@gmail.com>
2022-08-30 16:36:27 +0900

    Add RBDL_Simple_Example

--- C++/Shared_Memory/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yunjin/Documents/Code_Example/C++/Shared_Memory
-# Build directory: /home/yunjin/Documents/Code_Example/C++/Shared_Memory/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
