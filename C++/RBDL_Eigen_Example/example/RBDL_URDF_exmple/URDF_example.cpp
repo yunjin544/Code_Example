377c760a6388e630f331069da6ab8a4a1ce76967
yunjin <yunjin544>
2022-11-29 11:04:34 +0900

    add pinocchio code Cmakelist templete

--- C++/RBDL_Eigen_Example/example/RBDL_URDF_exmple/URDF_example.cpp
+++ C++/RBDL_Eigen_Example/example/RBDL_URDF_exmple/URDF_example.cpp
@@ -5,9 +5,10 @@
 #include <rbdl/rbdl_utils.h>
 #include <rbdl/addons/urdfreader/urdfreader.h>
 #include <stdio.h>
-#include <time.h>
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
 
 
 int main (int argc, char* argv[]) {
@@ -42,10 +43,24 @@ int main (int argc, char* argv[]) {
 
     while (true)
     {
+        system_clock::time_point start = system_clock::now();
+
         Q = RigidBodyDynamics::Math::VectorNd::Random((size_t)robotModel.dof_count);
         RigidBodyDynamics::UpdateKinematicsCustom(robotModel, &Q, NULL, NULL);
         RigidBodyDynamics::CalcPointJacobian(robotModel, Q, end_effector_id, pos, J, false);
         end_eff.segment<3>(0) = RigidBodyDynamics::CalcBodyToBaseCoordinates(robotModel, Q, end_effector_id, pos, false);
+        //RigidBodyDynamics::InverseKinematics(robotModel,);
+        system_clock::time_point end = system_clock::now();
+        nanoseconds mill = duration_cast<nanoseconds>(end - start);
+
+        std::cout << "경과시간(ns) : " << mill.count() << endl;
+        std::cout << "Jacobian" << std::endl;
+        std::cout << J << std::endl;
+
+        std::cout << "end_effector position" << std::endl;
+        std::cout << "end_effector x : " << end_eff(0) << std::endl;
+        std::cout << "end_effector y : " << end_eff(1) << std::endl;
+        std::cout << "end_effector z : " << end_eff(2) << std::endl;
 
     }
 
